c245c1b1ffdb68afebdd35bca8c07f39d7f61905
Ranjithkumar0 <ranjithkumar@sedintechnologies.com>
2022-10-13 16:50:03 +0530

    created login and signup validations Instruments form page with drop down menu

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_12_121652) do
+ActiveRecord::Schema.define(version: 2022_10_13_065137) do
 
   create_table "instruments", force: :cascade do |t|
     t.string "brand"
@@ -22,6 +22,8 @@ ActiveRecord::Schema.define(version: 2022_10_12_121652) do
     t.decimal "price", precision: 5, scale: 2, default: "0.0"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "image"
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -30,6 +32,11 @@ ActiveRecord::Schema.define(version: 2022_10_12_121652) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "name"
